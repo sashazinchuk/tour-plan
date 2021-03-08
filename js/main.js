51dc08e1202b7597955183005c02b54a02f746f7
sashazinchuk <sashazinchuk23@gmail.com>
2021-03-08 11:38:55 +0200

    add: slider & styles

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button_next',
+    prevEl: '.slider-button_prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
