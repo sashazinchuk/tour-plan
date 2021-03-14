e737d9094ba56254d73f622a85a27e9493a2a02b
sashazinchuk <sashazinchuk23@gmail.com>
2021-03-14 17:56:09 +0200

    замена файлов и разметка

--- js/main.js
+++ js/main.js
@@ -12,14 +12,3 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
-
-const swiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button_next',
-    prevEl: '.reviews-slider__button_prev',
-  },
-});
