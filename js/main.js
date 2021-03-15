fb253afcde5a08606215187c0f2dd4c9925ab726
sashazinchuk <sashazinchuk23@gmail.com>
2021-03-15 10:30:20 +0200

    add: стили для reviews-slider (кнопки не работают)

--- js/main.js
+++ js/main.js
@@ -12,3 +12,14 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+
+const swiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button_next',
+    prevEl: '.reviews-slider__button_prev',
+  },
+});
